bcbc1403882cfcfd52d61892fc502ac0882a25bc
Hasin Hayder <hasin@leevio.com>
2023-07-31 11:15:43 +0600

    Episode 06 Code

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,41 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
-const saarcCountries = ref([
-  {name:"Bangladesh",capital:"Dhaka"},
-  {name:"Nepal",capital:"Kathmandu"},
-  {name:"Bhutan",capital:"Thimphu"},
-  {name:"Sri Lanka",capital:"Colombo"},
-  {name:"India",capital:"New Delhi"},
-  {name:"Pakistan",capital:"Islamabad"},
-  {name:"Maldives",capital:"Male"}
+const persons = reactive([
+  { name: 'John Doe', age: 25, email: 'john@doe.com' },
+  { name: 'Jane Doe', age: 26, email: 'jane@doe.com' },
+  { name: 'John Smith', age: 27, email: 'john@smith.com' },
+  { name: 'Jane Smith', age: 28, email: 'jane@smith.com' }
 ])
 
+const activeIndex = ref(0)
 
-const getCapitals = computed(()=>{
-  let capitals = [];
-  saarcCountries.value.forEach(country => {
-    capitals.push(country.capital)
-  })
-  return capitals.sort(()=> Math.random() - 0.5);
-})
-
-const getScore = computed(()=>{
-  let score = 0;
-  saarcCountries.value.forEach(country => {
-    if(country.answer === country.capital){
-      score++;
-    }
-  })
-  return score;
-})
-
+function getActivePerson() {
+  return persons[activeIndex.value]
+}
 </script>
 
 <template>
   <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">{{ saarcCountries }}</p>
-    <p>Your Score: {{ getScore }}</p>
-    <div class="mb-5 mt-5" v-for="country in saarcCountries">
-      <p>What is the capital of {{ country.name }}</p>
-      <template v-for="capital in getCapitals"> 
-      <input type="radio" :name="country.name"  :value="capital" v-model="country.answer" />
-      <label class="ml-2 mr-2">{{ capital }}</label>
-      </template>
+    <p class="my-5">{{ persons }}</p>
+    <div class="flex ">
+      <div class="w-1/2">
+        <p>{{ activeIndex }}</p>
+        
+        <p class="mb-10">
+        <h2>Select a person</h2>
+        <select name="person" id="person" v-model="activeIndex">
+          <option v-for="(person, index) in persons" :key="index" :value="index">{{ person.name }}</option>
+        </select>
+        </p>
+      </div>
+      <div>
+        <div>
+          <input class="p-5 my-3" type="text" v-for="(v, k, i) in persons[activeIndex]" :key="i" :placeholder="k" v-model="persons[activeIndex][k]">
+        </div>
+      </div>
     </div>
+
   </section>
 </template>
 <style scoped></style>
