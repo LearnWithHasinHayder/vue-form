7f8a7ba47f1112e1d3c6c98522c26ace7f9b74e0
Hasin Hayder <hasin@leevio.com>
2023-07-31 20:49:52 +0600

    Episode 08 Code

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
-import { countries } from './data/countries.js'
-const search = ref('')
 
-function getFilteredCountries() {
-  return countries.filter(country => {
-    return country.name.toLowerCase().startsWith(search.value.toLowerCase())
-  })
-}
 </script>
 
 <template>
-  <section class="mx-auto container text-left">
-    <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="my-5">{{ search }}</p>
-    <p class="my-5">
-      search: <input class="p-3" type="text" v-model="search">
-    </p>
-    <p class="mt-10">
-    <ul>
-      <li v-for="country in getFilteredCountries()" :key="country.code">
-        {{ country.name }}
-      </li>
-    </ul>
-    </p>
-
-
+  <section class="flex h-screen w-full">
+    <div class="w-1/2" style="background-image: url(https://images.unsplash.com/photo-1690555575753-7aa27df96b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80); background-repeat: no-repeat; background-size: cover;">
+      <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos!</h1>
+    </div>
+    <div class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
+      <h2 class="mb-5 text-xl">Login or register</h2>
+      <div class="w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+              Username
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+          </div>
+          <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+              Password
+            </label>
+            <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+            <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Sign In
+            </button>
+            <a class="inline-block align-baseline font-bold text-sm text-orange-600 hover:text-orange-800" href="#">
+              Or Register
+            </a>
+          </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    </div>
   </section>
 </template>
 <style scoped></style>
