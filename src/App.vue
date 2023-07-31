1e14d53695a2c1afe19ab5eae10f4e4930d287d2
Hasin Hayder <countdraculla@gmail.com>
2023-07-31 15:12:39 +0600

    Episode 02 Code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive } from 'vue'
 const person = reactive({
   name: 'John Doe',
   age: 30,
   job: 'Web Developer',
 })
+
 </script>
 
 <template>
@@ -14,7 +15,12 @@ const person = reactive({
       {{ person }}
     </p>
     
-    <div class="flex flex-col mb-5" v-for="(value,key,index) in person" :key="index">
+    <!-- <div class="flex flex-col mb-5 text-left" >
+      <label for="name">Name</label>
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person.name" />
+    </div> -->
+
+    <div class="flex flex-col mb-5 text-left" v-for="(value,key,index) in person" :key="key" >
       <label for="name">{{key}}</label>
       <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]" />
     </div>
