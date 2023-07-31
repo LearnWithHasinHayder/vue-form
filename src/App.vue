126fb166c8e244a834b2b823119d0c268e736af0
Hasin Hayder <countdraculla@gmail.com>
2023-07-31 17:12:24 +0600

    Episode 05 Code

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
-const saarcCountries = ref([
-  {name:"Bangladesh",capital:"Dhaka"},
-  {name:"Nepal",capital:"Kathmandu"},
-  {name:"Bhutan",capital:"Thimphu"},
-  {name:"Sri Lanka",capital:"Colombo"},
-  {name:"India",capital:"New Delhi"},
-  {name:"Pakistan",capital:"Islamabad"},
-  {name:"Maldives",capital:"Male"}
+import { reactive } from 'vue'
+const saarcCountries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Nepal", capital: "Kathmandu" },
+  { name: "Bhutan", capital: "Thimphu" },
+  { name: "Sri Lanka", capital: "Colombo" },
+  { name: "India", capital: "New Delhi" },
+  { name: "Pakistan", capital: "Islamabad" },
+  { name: "Maldives", capital: "Male" }
 ])
 
+const capitals = ["Dhaka", "Kathmandu", "Thimphu", "Colombo", "New Delhi", "Islamabad", "Male"]
 
-const getCapitals = computed(()=>{
-  let capitals = [];
-  saarcCountries.value.forEach(country => {
-    capitals.push(country.capital)
-  })
-  return capitals.sort(()=> Math.random() - 0.5);
-})
 
-const getScore = computed(()=>{
-  let score = 0;
-  saarcCountries.value.forEach(country => {
+function getRandomCapitals(){
+  return capitals;
+  //return capitals.sort(()=>Math.random() - 0.5)
+}
+
+function getScore(){
+  let score = 0
+  saarcCountries.forEach(country => {
     if(country.answer === country.capital){
-      score++;
+      score++
     }
   })
-  return score;
-})
+  return score
+}
 
 </script>
 
@@ -35,12 +34,12 @@ const getScore = computed(()=>{
   <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
     <p class="mb-10">{{ saarcCountries }}</p>
-    <p>Your Score: {{ getScore }}</p>
-    <div class="mb-5 mt-5" v-for="country in saarcCountries">
-      <p>What is the capital of {{ country.name }}</p>
-      <template v-for="capital in getCapitals"> 
-      <input type="radio" :name="country.name"  :value="capital" v-model="country.answer" />
-      <label class="ml-2 mr-2">{{ capital }}</label>
+    <p>Your Score: {{ getScore() }}</p>
+    <div class="my-5 border border-gray-400 p-5" v-for="(country, index) in saarcCountries" :key="country.name">
+      <p>What is the capital of {{ country.name }}?</p>
+      <template v-for="capital in capitals">
+        <input :name="country.name" type="radio" :value="capital" v-model="country.answer" />
+        <label class="ml-2 mr-2">{{capital}}</label>
       </template>
     </div>
   </section>
