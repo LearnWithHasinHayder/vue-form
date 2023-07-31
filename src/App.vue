ccb0d7107c3079a44628c5be3816c9c3fc7228e8
Hasin Hayder <countdraculla@gmail.com>
2023-07-31 14:43:12 +0600

    Episode 01 Code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
-const color = ref('')
-const getStyle = computed(() => {
+import { ref } from 'vue'
+const color = ref('None')
+
+function getStyle() {
   return {
     backgroundColor: color.value,
   }
-})
+}
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <span v-show="color">Your Favorite Color: {{ color }}</span>
+    <p class="pb-5">Your Favorite Color: {{ color }}</p>
     <p>
-      Your Favorite Color: <input class="p-5" v-model="color" />
+      Your Favorite Color: 
+      <input class="p-5" v-model="color" />
+      <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
     </p>
-    <div class="w-32 h-32 mt-10" :style="getStyle"></div>
   </section>
 </template>
 <style scoped></style>
