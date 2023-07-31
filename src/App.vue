8fbd856b223b45d2f29e32127903cad1ccb57a9a
Hasin Hayder <hasin@leevio.com>
2023-07-31 10:13:14 +0600

    Episode 03 Code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
-const person = reactive({
-  name: 'John Doe',
-  age: 30,
-  job: 'Web Developer',
-})
+import { ref, reactive } from 'vue'
+const displayImage = ref(true)
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">
-      {{ person }}
-    </p>
-    
-    <div class="flex flex-col mb-5" v-for="(value,key,index) in person" :key="index">
-      <label for="name">{{key}}</label>
-      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]" />
-    </div>
+    <p class="mb-10">{{ displayImage }}</p>
+    <label for="">Display Random Image</label>
+    <input class="ml-2" type="checkbox" v-model="displayImage" />
+    <img
+      v-if="displayImage"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random/?version=${Math.random()}`"
+      alt=""  />
 
   </section>
 </template>
