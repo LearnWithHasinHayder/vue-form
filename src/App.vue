963ce95190e6c43441170444681313a6f82311f4
Hasin Hayder <countdraculla@gmail.com>
2023-07-31 15:42:29 +0600

    Episode 03 Code

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,22 @@ const displayImage = ref(true)
     <h1 class="text-2xl mb-10">Vue Form</h1>
     <p class="mb-10">{{ displayImage }}</p>
     <label for="">Display Random Image</label>
-    <input class="ml-2" type="checkbox" v-model="displayImage" />
+    <input class="ml-2" type="checkbox" v-model="displayImage"  />
+    <p class="mt-5">
+      <input type="radio" name="display" :value="true" v-model="displayImage"> On <br/>
+      <input type="radio" name="display" :value="false" v-model="displayImage"> Off <br/>
+    </p>
     <img
+      v-show="displayImage"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random`"
+      alt=""  />
+      
+      <!-- <img
       v-if="displayImage"
       class="mt-10 mx-auto w-[500px]"
-      :src="`https://source.unsplash.com/random/?version=${Math.random()}`"
-      alt=""  />
+      :src="`https://source.unsplash.com/random?version=${Math.random()}`"
+      alt=""  /> -->
 
   </section>
 </template>
