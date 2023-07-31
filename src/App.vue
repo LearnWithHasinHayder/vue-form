10e175c430fda5045b57c9fd1625d3e2103d5eb9
Hasin Hayder <hasin@leevio.com>
2023-07-31 11:29:05 +0600

    Episode 07 Code

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,30 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
-const persons = reactive([
-  { name: 'John Doe', age: 25, email: 'john@doe.com' },
-  { name: 'Jane Doe', age: 26, email: 'jane@doe.com' },
-  { name: 'John Smith', age: 27, email: 'john@smith.com' },
-  { name: 'Jane Smith', age: 28, email: 'jane@smith.com' }
-])
+import { countries } from './data/countries.js'
+const search = ref('')
 
-const activeIndex = ref(0)
-
-function getActivePerson() {
-  return persons[activeIndex.value]
+function getFilteredCountries() {
+  return countries.filter(country => {
+    return country.name.toLowerCase().startsWith(search.value.toLowerCase())
+  })
 }
 </script>
 
 <template>
   <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="my-5">{{ persons }}</p>
-    <div class="flex ">
-      <div class="w-1/2">
-        <p>{{ activeIndex }}</p>
-        
-        <p class="mb-10">
-        <h2>Select a person</h2>
-        <select name="person" id="person" v-model="activeIndex">
-          <option v-for="(person, index) in persons" :key="index" :value="index">{{ person.name }}</option>
-        </select>
-        </p>
-      </div>
-      <div>
-        <div>
-          <input class="p-5 my-3" type="text" v-for="(v, k, i) in persons[activeIndex]" :key="i" :placeholder="k" v-model="persons[activeIndex][k]">
-        </div>
-      </div>
-    </div>
+    <p class="my-5">{{ search }}</p>
+    <p class="my-5">
+      search: <input class="p-3" type="text" v-model="search">
+    </p>
+    <p class="mt-10">
+    <ul>
+      <li v-for="country in getFilteredCountries()" :key="country.code">
+        {{ country.name }}
+      </li>
+    </ul>
+    </p>
+
 
   </section>
 </template>
